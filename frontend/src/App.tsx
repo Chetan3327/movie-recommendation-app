f686609d7cca4f3f14d5d787fced4b6052f886ec
Chetan3327 <chauhanchetan12789@gmail.com>
2024-02-25 23:01:22 +0530

    workend on frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,87 @@
+import axios, { AxiosResponse } from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+type MovieData = {
+    original_title: any;
+    poster_path: any;
+}
+
+const MovieCard = (movie: MovieData) => {
+  return(
+    <div>
+      <img className='rounded-md shadow-md cursor-pointer' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={250} alt={movie.original_title} />
+    </div>
+  )
+}
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("")
+  const [movieData, setMovieData] = useState<MovieData[]>([])
+  const [errorMessage, setErrorMessage] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?with_genres=28&api_key=${TMDB_API_KEY}&with_origin_country=IN&page=1`)
+      const movieDataArray = data.results.map((movie_data: MovieData) => ({
+        "original_title": movie_data.original_title,
+        "poster_path": movie_data.poster_path,
+      }));
+      setMovieData(movieDataArray)
+    }
+    fetchData();
+  }, [])
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+    setErrorMessage("")
+  }
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+    if(!searchTerm){
+      return 
+    }
+    setLoading(true)
+    try {
+      const response = await axios.post(`${BACKEND_URL}/recommend`, {"sentence": searchTerm})
+      const recommendations: number[] = response.data.recommendations
+      const axiosRequests: Promise<AxiosResponse<any, any>>[] = recommendations.map((id) => {
+        return axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_API_KEY}&language=en-US`)
+      })
+
+      Promise.all(axiosRequests).then((responses) => {
+        const movieDataArray = responses.map((movie_data) => ({
+          "original_title": movie_data.data.original_title,
+          "poster_path": movie_data.data.poster_path,
+        }));
+        setMovieData(movieDataArray);
+        console.log(movieDataArray)
+      }).catch((error) => {
+        console.error("Error fetching movie data:", error);
+      });
+    } catch (error) {
+      console.error("Error fetching recommendations:", error);
+      setErrorMessage("Error fetching recommendations. Please try again later.");
+    }
+    setLoading(false)
+    setErrorMessage("")
+  }
+  return (
+    <div className='bg-primary min-h-screen text-neutral-50 p-20'>
+      <form className='flex gap-5'>
+        <input onChange={(e) => handleInput(e)} className='w-full bg-secondary p-2 rounded outline-none' type="text" placeholder='search by keyword, genre' />
+        <button onClick={(e) => handleSubmit(e)} className={`${loading && 'animate-spin'} bg-secondary px-4 p-2 rounded hover:bg-secondary/50 hover:duration-400`}>Recommend</button>
+      </form>
+      {errorMessage && (<div className='my-5 pl-5 bg-red-500 p-2 rounded-md border-2 border-red-950'>{errorMessage}</div>)}
+      <div className='mt-10 flex flex-wrap gap-5 justify-center'>
+        {movieData.map((movie, idx) => {
+          return(<MovieCard {...movie} key={idx} />)
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#10141e',
+        secondary: '#0c1a31'
+      }
+    },
   },
   plugins: [],
 }
